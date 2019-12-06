78d8ac95b686290958d19a68dd3c13c206ce401f
eugeny-m <evgenii.maksimov@yahoo.com>
2019-12-06 18:03:06 +0300

    add views

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from pizza.models import Pizza, Order, OrderedPizza
+from pizza.serializers import (
+    OrderSerializer,
+    OrderedPizzaSerializer,
+    PizzaSerializer,
+    PizzaPriceSerializer)
+
+
+class PizzaViewSet(viewsets.ModelViewSet):
+    serializer_class = PizzaSerializer
+    queryset = Pizza.objects.all()
+
+
+class PizzaPriceViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin
+):
+    serializer_class = PizzaPriceSerializer
+    queryset = Pizza.objects.all()
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class OrderedPizzaViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderedPizzaSerializer
+    queryset = OrderedPizza.objects.all()
